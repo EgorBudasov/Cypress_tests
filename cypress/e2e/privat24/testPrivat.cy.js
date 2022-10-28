162667c83ca572796c1d4cb437b8eece1f8042b1
Egor <egor.budasow@gmail.com>
2022-10-28 13:27:09 +0300

    firstGetRequstSypress

--- cypress/e2e/privat24/testPrivat.cy.js
+++ cypress/e2e/privat24/testPrivat.cy.js
@@ -23,7 +23,7 @@ it('Test transver', () => {
 
 });
 
-it('mobile payment', () => {
+it.only('mobile payment', () => {
     cy.visit('https://next.privat24.ua/mobile');
 
     mobilePayment.typePhone('992374432');
@@ -38,4 +38,7 @@ it('mobile payment', () => {
     
 
 });
+ it('Api test', () =>{
 
+    
+ })
